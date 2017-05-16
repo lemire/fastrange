11bb5c19b05c1b4c2ea754322ddd68d928424821
Daniel Lemire <lemire@gmail.com>
2017-05-15 20:06:37 -0400

    updating faulty function

--- tests/cpp/tests.cpp
+++ tests/cpp/tests.cpp
@@ -12,8 +12,7 @@
 /////////////////////
 
 static uint32_t get32rand() {
-    return (((uint32_t)rand() << 0) & 0x0000FFFFul) |
-           (((uint32_t)rand() << 16) & 0xFFFF0000ul);
+    return (rand() ^ (rand() << 15) ^ (rand() << 30));
 }
 
 static uint64_t get64rand() {
